17649789f91aabd77e801267439535acfe4e1ea5
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2023-01-24 17:53:33 +0530

    First commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import titlelogo from './titlelogo.png';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+  titlelogo,
+};
